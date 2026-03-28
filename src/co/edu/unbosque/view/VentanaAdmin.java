9d1c2b84f3e8844ae379f47e7e5dd837a36a1d5c
DynaMightt327 <malejacarvajalnepta@gmail.com>
2026-03-28 03:32:27 -0500

    otro

--- src/co/edu/unbosque/view/VentanaAdmin.java
+++ src/co/edu/unbosque/view/VentanaAdmin.java
@@ -1,9 +1,12 @@
 package co.edu.unbosque.view;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class VentanaAdmin extends JFrame {
 	
@@ -14,6 +17,10 @@ public class VentanaAdmin extends JFrame {
 	private JButton actualizar;
 	private JButton eliminar;
 	private JButton volver;
+	
+	private JPanel banner;
+	private JLabel titulo;
+	
 	public VentanaAdmin() {
 		initcomps();
 		setVisible(false);
@@ -27,12 +34,44 @@ public class VentanaAdmin extends JFrame {
 		this.setResizable(false);
 		this.setLayout(null);
 		this.getContentPane().setBackground(Color.decode("#fff4e4"));
+		
+		banner = new JPanel();
+		banner.setBounds(0, 0, 980, 100);
+		banner.setBackground(Color.white);
+		add(banner);
+		
+		titulo = new JLabel("Veterinaria");
+		titulo.setBounds(450, 0, 300, 100);
+		titulo.setForeground(Color.black);
+		titulo.setFont(new Font("Arial", Font.BOLD, 70));
+		banner.add(titulo);
 
 		panelCrear = new PanelCrear();
 		panelCrear.setVisible(true);
-		
 		add(panelCrear);
 		
-
+		crear = new JButton("Crear");
+		crear.setBounds(15, 190, 140, 30);
+		crear.setBorderPainted(false);
+		crear.setForeground(Color.decode("#060b37"));
+		add(crear);
+		
+		mostrar = new JButton("Mostrar");
+		mostrar.setBounds(15, 230, 140, 30);
+		mostrar.setBorderPainted(false);
+		mostrar.setForeground(Color.decode("#060b37"));
+		add(mostrar);
+		
+		actualizar = new JButton("Actualizar");
+		actualizar.setBounds(15, 270, 140, 30);
+		actualizar.setBorderPainted(false);
+		actualizar.setForeground(Color.decode("#060b37"));
+		add(actualizar);
+		
+		eliminar = new JButton("Eliminar");
+		eliminar.setBounds(15, 310, 140, 30);
+		eliminar.setBorderPainted(false);
+		eliminar.setForeground(Color.decode("#060b37"));
+		add(eliminar);
 	}
 }
